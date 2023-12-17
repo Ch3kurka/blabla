0ab618d25cf7f143372e5de18d271ed9fab993db
oleg <moonnoon0909@gmail.com>
2023-12-17 21:04:04 +0300

    ssddfggh

--- newapp/views.py
+++ newapp/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here
 from django.shortcuts import render
-def base (request):
+
+
+def base(request):
     return render(request, 'base.html', locals())
